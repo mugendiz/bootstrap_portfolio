1d2fb358cf156205ff9f22f32897f390651e8185
Andrew Zoka <mugendiz@gmail.com>
2018-05-29 00:57:25 -0400

    made adjustments to css; consolidated pages to one, pending handle page links

---
+++ assets/js/pageEffects.js
@@ -0,0 +1,15 @@
+
+// scroll to specific area of page
+
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: smooth
+})
+
+// Scroll certain amounts from current position
+window.scrollBy({
+  top: 100, 
+  left: 0,
+  behavior: 'smooth'
+});
