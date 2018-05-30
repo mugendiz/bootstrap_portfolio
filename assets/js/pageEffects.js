a1cfdc7842a12a2eb0804844aa6cbca9fc294e57
Andrew Zoka <mugendiz@gmail.com>
2018-05-30 04:05:46 -0400

    style modifications

--- assets/js/pageEffects.js
+++ assets/js/pageEffects.js
@@ -1,15 +1,36 @@
+//
+// // scroll to specific area of page
+//
+// window.scroll({
+//   top: 2500,
+//   left: 0,
+//   behavior: smooth
+// })
+//
+// // Scroll certain amounts from current position
+// window.scrollBy({
+//   top: 100,
+//   left: 0,
+//   behavior: 'smooth'
+// });
+//
+// // Scroll to certain element
+//
+// document.querySelector()
 
-// scroll to specific area of page
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-window.scroll({
-  top: 2500,
-  left: 0,
-  behavior: smooth
-})
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
 
-// Scroll certain amounts from current position
-window.scrollBy({
-  top: 100, 
-  left: 0,
-  behavior: 'smooth'
-});
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
